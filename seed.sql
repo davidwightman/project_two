a71eedeb25760553777d72907e922fc8a68f4cbc
wightman <davidwightmanmusic@gmail.com>
2015-08-17 06:57:16 -0400

    new content

--- seed.sql
+++ seed.sql
@@ -1,5 +1,5 @@
 INSERT INTO user (user_name, user_avatar) VALUES ("Joe", "http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2012/6/12/1339518379773/TROLLS---1992-008.jpg");
 
-INSERT INTO thread(author_id, title, content, votes) VALUES (1, "New Post", "This is a new post.", 1);
+INSERT INTO thread(author_id, title, content, votes) VALUES (1, "New Post", "This is the first post to this forum. I'm excited to be here.", 1);
 
 
